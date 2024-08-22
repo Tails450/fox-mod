704f4617ac754021a7168437a41c0c6f5854eca6
Tails450 <93626623+Tails450@users.noreply.github.com>
2024-08-22 16:39:09 +0000

    u

--- your-scratch-extension/index.js
+++ your-scratch-extension/index.js
@@ -13,90 +13,61 @@ class Scratch3YourExtension {
      */
     getInfo () {
         return {
-            // unique ID for your extension
-            id: 'mathPlus',
-
-            // name that will be displayed in the Scratch UI
-            name: 'Math+',
-
-            // colours to use for your extension blocks
-            color1: '#07acff',
-            color2: '#0a72ff',
-
-            // icons to display
-            blockIconURI: 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAkAAAAFCAAAAACyOJm3AAAAFklEQVQYV2P4DwMMEMgAI/+DEUIMBgAEWB7i7uidhAAAAABJRU5ErkJggg==',
-            menuIconURI: 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAkAAAAFCAAAAACyOJm3AAAAFklEQVQYV2P4DwMMEMgAI/+DEUIMBgAEWB7i7uidhAAAAABJRU5ErkJggg==',
-
-            // your Scratch blocks
-            blocks: [
-                {
-                    // name of the function where your block code lives
-                    opcode: 'plus',
-
-                    // type of block - choose from:
-                    //   BlockType.REPORTER - returns a value, like "direction"
-                    //   BlockType.BOOLEAN - same as REPORTER but returns a true/false value
-                    //   BlockType.COMMAND - a normal command block, like "move {} steps"
-                    //   BlockType.HAT - starts a stack if its value changes from false to true ("edge triggered")
-                    blockType: BlockType.REPORTER,
-
-                    // label to display on the block
-                    text: '[n1] + [n2]',
-
-                    // true if this block should end a stack
-                    terminal: false,
-
-                    // where this block should be available for code - choose from:
-                    //   TargetType.SPRITE - for code in sprites
-                    //   TargetType.STAGE  - for code on the stage / backdrop
-                    // remove one of these if this block doesn't apply to both
-                    filter: [ TargetType.SPRITE, TargetType.STAGE ],
-
-                    // arguments used in the block
-                    arguments: {
-                        n1: {
-                            // default value before the user sets something
-                            defaultValue: '1',
-
-                            // type/shape of the parameter - choose from:
-                            //     ArgumentType.ANGLE - numeric value with an angle picker
-                            //     ArgumentType.BOOLEAN - true/false value
-                            //     ArgumentType.COLOR - numeric value with a colour picker
-                            //     ArgumentType.NUMBER - numeric value
-                            //     ArgumentType.STRING - text value
-                            //     ArgumentType.NOTE - midi music value with a piano picker
-                            type: ArgumentType.NUMBER
-                        },
-                        n2: {
-                            // default value before the user sets something
-                            defaultValue: '1',
-
-                            // type/shape of the parameter - choose from:
-                            //     ArgumentType.ANGLE - numeric value with an angle picker
-                            //     ArgumentType.BOOLEAN - true/false value
-                            //     ArgumentType.COLOR - numeric value with a colour picker
-                            //     ArgumentType.NUMBER - numeric value
-                            //     ArgumentType.STRING - text value
-                            //     ArgumentType.NOTE - midi music value with a piano picker
-                            type: ArgumentType.NUMBER
-                        }
-                    }
+          // unique ID for your extension
+          id: 'yourScratchExtension',
+      
+          // name displayed in the Scratch UI
+          name: 'Demo',
+      
+          // colours to use for your extension blocks
+          color1: '#000099',
+          color2: '#660066',
+      
+          // your Scratch blocks
+          blocks: [
+            {
+              // function where your code logic lives
+              opcode: 'myFirstBlock',
+      
+              // type of block
+              blockType: BlockType.REPORTER,
+      
+              // label to display on the block
+              text: 'Title for ISBN book [BOOK_NUMBER]',
+      
+              // arguments used in the block
+              arguments: {
+                BOOK_NUMBER: {
+                  defaultValue: 1718500564,
+      
+                  // type/shape of the parameter
+                  type: ArgumentType.NUMBER
                 }
-            ]
+              }
+            }
+          ]
         };
-    }
+      }
 
 
     /**
      * implementation of the block with the opcode that matches this name
      *  this will be called when the block is used
      */
-    plus ({ n1, n2 }) {
-        // example implementation to return a string
-        let product = n1 + n2
-        parseFloat(product)
-        return product;
-    }
+    myFirstBlock ({ BOOK_NUMBER }) {
+        return fetch('https://openlibrary.org/isbn/' + BOOK_NUMBER + '.json')
+          .then((response) => {
+            if (response.ok) {
+              return response.json();
+            }
+            else {
+              return { title: 'Unknown' };
+            }
+          })
+          .then((bookinfo) => {
+            return bookinfo.title;
+          });
+      }
 }
 
 module.exports = Scratch3YourExtension;
